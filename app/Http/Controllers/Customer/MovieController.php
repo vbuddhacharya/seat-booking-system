789b194d5431c961d67330eec519347a6d40c7c9
vbuddhacharya <vishabuddhacharya01@gmail.com>
2024-05-06 13:13:14 +0545

    SBS-10: fix: updated index to show sessions

--- app/Http/Controllers/Customer/MovieController.php
+++ app/Http/Controllers/Customer/MovieController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers\Customer;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Movie;
+use App\Models\TheatreSession;
+use Carbon\Carbon;
 
 class MovieController extends Controller
 {
     public function index(){
-        $movies = Movie::paginate(10);
-        return view('index', compact('movies'));
+        $theatreSession = TheatreSession::where('date','>=', Carbon::now()->format('Y-m-d'))->where('start_time','>=', Carbon::now()->format('H:i:s'))->paginate(10);
+        return view('index', compact('theatreSession'));
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,14 +2,14 @@
 
 @section('customer-content')
 <div class="flex flex-row flex-wrap justify-between">
-    @foreach ($movies as $movie)
+    @foreach ($theatreSession as $session)
         <div class="card card-compact w-3/12 bg-base-100 shadow-xl m-2">
             <figure>
-                <img src="{{ url('storage/images/' . $movie->poster) }}">
+                <img src="{{ url('storage/images/' . $session->movie->poster) }}">
             </figure>
             <div class="card-body">
-                <h2 class="card-title">{{$movie->name}}</h2>
-                <p>{{$movie->release_date}}</p>
+                <h2 class="card-title">{{$session->movie->name}}</h2>
+                <p>{{$session->movie->release_date}}</p>
                 <div class="card-actions justify-end">
                     <button class="btn btn-primary">Get Tickets</button>
                 </div>
